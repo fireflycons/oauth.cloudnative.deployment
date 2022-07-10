6fac4e112a0284a60549571d8136cb30ec4c8cbf
gary <gary@MACSTATION.local>
2022-07-10 20:32:30 +0100

    Updates

--- 3-build.sh
+++ 3-build.sh
@@ -31,11 +31,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# TODO: delete after merge
-cd finalspa
-git checkout feature/deployment
-cd ..
-
 ./finalspa/deployment/kubernetes-local/build.sh
 if [ $? -ne 0 ]; then
   echo '*** Final SPA build problem encountered'
@@ -71,11 +66,6 @@ elif [ "$API_TECH" == 'java' ]; then
   fi
 fi
 
-# TODO: delete after merge
-cd finalapi
-git checkout feature/deployment
-cd ..
-
 ./finalapi/deployment/kubernetes-local/build.sh
 if [ $? -ne 0 ]; then
   echo '*** Final API build problem encountered'
@@ -91,11 +81,6 @@ if [ $? -ne 0 ]; then
   echo '*** Token handler download problem encountered'
   exit 1
 fi
-
-# TODO: delete after merge
-cd tokenhandler
-git checkout feature/deployment
-cd ..
 
 ./tokenhandler/deployment/kubernetes-local/build.sh
 if [ $? -ne 0 ]; then
