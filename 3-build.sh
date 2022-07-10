aacda56d5821bf09a014d51d17b06832239b7494
gary <gary@MACSTATION.local>
2022-07-10 19:05:40 +0100

    Updates

--- 3-build.sh
+++ 3-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-########################################################################
-# This builds application ode into Docker containers ready for deploying
-########################################################################
+#########################################################################
+# This builds application code into Docker containers ready for deploying
+#########################################################################
 
 #
 # Ensure that we are in the folder containing this script
@@ -22,33 +22,85 @@ else
 fi
 
 #
-# Build SPA resources
+# Build Web Host and SPA resources
 #
-./finalspa-scripts/build.sh
+#rm -rf finalspa 2>/dev/null
+#git clone https://github.com/gary-archer/oauth.websample.final finalspa
+#if [ $? -ne 0 ]; then
+#  echo '*** Final SPA download problem encountered'
+#  exit 1
+#fi
+
+# TODO: delete after merge
+#cd finalspa
+#git checkout feature/deployment
+#cd ..
+
+#./finalspa/deployment/kubernetes-local/build.sh
+#if [ $? -ne 0 ]; then
+#  echo '*** Final SPA build problem encountered'
+#  exit 1
+#fi
+
+#
+# Build API resources
+#
+rm -rf finalapi 2>/dev/null
+if [ "$API_TECH" == 'nodejs' ]; then
+  
+  git clone https://github.com/gary-archer/oauth.apisample.nodejs finalapi
+  if [ $? -ne 0 ]; then
+    echo '*** Node.js API download problem encountered'
+    exit 1
+  fi
+
+elif [ "$API_TECH" == 'netcore' ]; then
+
+  git clone https://github.com/gary-archer/oauth.apisample.netcore finalapi
+  if [ $? -ne 0 ]; then
+    echo '*** .NET API download problem encountered'
+    exit 1
+  fi
+
+elif [ "$API_TECH" == 'java' ]; then
+
+  git clone https://github.com/gary-archer/oauth.apisample.javaspringboot finalapi
+  if [ $? -ne 0 ]; then
+    echo '*** Java API download problem encountered'
+    exit 1
+  fi
+fi
+
+# TODO: delete after merge
+cd finalapi
+git checkout feature/deployment
+cd ..
+
+./finalapi/deployment/kubernetes-local/build.sh
 if [ $? -ne 0 ]; then
+  echo '*** Final API build problem encountered'
+  exit 1
+fi
+echo 'EXITING BUILD EARLY'
+exit
+
+#
+# Build token handler components
+#
+rm -rf tokenhandler 2>/dev/null
+git clone https://github.com/gary-archer/oauth.tokenhandler.docker tokenhandler
+if [ $? -ne 0 ]; then
+  echo '*** Token handler download problem encountered'
   exit 1
 fi
 
-#
-# Build the API
-#
-./finalapi-scripts/build.sh "$API_TECH"
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+# TODO: delete after merge
+cd tokenhandler
+git checkout feature/deployment
+cd ..
 
-#
-# Build the OAuth Agent
-#
-./oauth-agent-scripts/build.sh
+./tokenhandler/deployment/kubernetes-local/build.sh
 if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-#
-# Build the reverse proxy
-#
-./reverseproxy/build.sh
-if [ $? -ne 0 ]; then
+  echo '*** Token Handler build problem encountered'
   exit 1
 fi
